52e3a9185aa5dc70a9927b3cd56556758b433735
ShamaKung <shamaturin@gmail.com>
2022-12-21 13:58:24 +0500

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите кол-во строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+for(int i = 0; i < m; i++)
+{
+    for(int j = 0; j < n; j++)
+    {
+        array[i, j] = new Random().NextDouble() * 100;
+        Console.Write("{0}\t", Math.Round(array[i, j], 3));
+    }
+    Console.WriteLine("");
+}
